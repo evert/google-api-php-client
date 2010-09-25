ee5c284ef1f733db8868e5a481977e56370e45aa
chabotc <chabotc@6c5fc399-1a2c-022f-82ce-feb0d0b4916c>
2010-09-25 14:55:27 +0000

    Added predictions api generation
    git-svn-id: http://google-api-php-client.googlecode.com/svn/trunk@26 6c5fc399-1a2c-022f-82ce-feb0d0b4916c

--- src/generator/generate.sh
+++ src/generator/generate.sh
@@ -6,3 +6,5 @@ php ./generate.php easyhybrid v1 > ../contrib/apiEasyhybridService.php
 php ./generate.php latitude v1 > ../contrib/apiLatitudeService.php
 php ./generate.php moderator v1 > ../contrib/apiModeratorService.php
 php ./generate.php translate v2 > ../contrib/apiTranslateService.php
+php ./generate.php prediction v1.1 > ../contrib/apiPredictionService.php
+
