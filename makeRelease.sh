ce58dcd74e819b49d49c2ad7b1a0e7fb1b68344b
chirags@google.com <chirags@google.com@6c5fc399-1a2c-022f-82ce-feb0d0b4916c>
2011-09-02 19:04:32 +0000

    Update the release version number to prepare for the 0.4 release.
    Remove unnecessary files from the release file.
    
    
    git-svn-id: http://google-api-php-client.googlecode.com/svn/trunk@200 6c5fc399-1a2c-022f-82ce-feb0d0b4916c

--- makeRelease.sh
+++ makeRelease.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-VERSION=0.3
+VERSION=0.4
 TMPDIR=/tmp/google-api-php-client
 RELFILE=/tmp/google-api-php-client-${VERSION}.tar.gz
 
@@ -11,6 +11,8 @@ cp -r * $TMPDIR
 cd $TMPDIR
 find . -name ".*" -exec rm -rf {} \; &>/dev/null
 find . -name "makeRelease.sh" -exec rm -rf {} \; &>/dev/null
+find . -name "local_*" -exec rm -rf {} \; &>/dev/null
+find . -name "static" -exec rm -rf {} \; &>/dev/null
 cd ..
 tar c google-api-php-client | gzip > $RELFILE
 rm -rf $TMPDIR
