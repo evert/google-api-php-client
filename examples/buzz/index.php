010dfe563316deb8ee4fc01578dac250a09ed92d
chirags@google.com <chirags@google.com@6c5fc399-1a2c-022f-82ce-feb0d0b4916c>
2011-09-21 22:51:21 +0000

    Add the Google+ sample application.
    git-svn-id: http://google-api-php-client.googlecode.com/svn/trunk@219 6c5fc399-1a2c-022f-82ce-feb0d0b4916c

--- examples/buzz/index.php
+++ examples/buzz/index.php
@@ -1,19 +1,12 @@
 <?php
 require_once "includes/header.php";
-
 require_once "../../src/apiClient.php";
 require_once "../../src/contrib/apiBuzzService.php";
-
 ?><div id="buzzStream">
 	<div class="left" style="float:left; width:49%">
   		<div class="buzzPost ui-corner-all" style="width:98%">
   			<a class="person" href="getPublicStream.php">Get the public stream</a>
   		</div>
-	</div>
-	<div class="right" style="float:right; width:49%">
-  		<div class="buzzPost ui-corner-all" style="width:98%">
-			<a class="person" href="addOrEditComment.php">Add or Edit comments</a>
-		</div>
 	</div>
 </div><?php
 require_once "includes/footer.php";
