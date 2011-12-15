fea819f6fc7b58e71575dadd165e31b4f45b5fbc
chirags@google.com <chirags@google.com@6c5fc399-1a2c-022f-82ce-feb0d0b4916c>
2011-12-15 22:59:05 +0000

    Add support for listing segments and management goals in the Google Analytics example app.
    git-svn-id: http://google-api-php-client.googlecode.com/svn/trunk@291 6c5fc399-1a2c-022f-82ce-feb0d0b4916c

--- examples/analytics/simple.php
+++ examples/analytics/simple.php
@@ -35,6 +35,12 @@ if ($client->getAccessToken()) {
   $accounts = $service->accounts->listManagementAccounts();
   print "<h1>Accounts</h1><pre>" . print_r($accounts, true) . "</pre>";
 
+  $segments = $service->segments->listManagementSegments();
+  print "<h1>Segments</h1><pre>" . print_r($segments, true) . "</pre>";
+
+  $goals = $service->goals->listManagementGoals("~all", "~all", "~all");
+  print "<h1>Segments</h1><pre>" . print_r($goals, true) . "</pre>";
+
 $_SESSION['token'] = $client->getAccessToken();
 } else {
   $authUrl = $client->createAuthUrl();
