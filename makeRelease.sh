58fbce7faf0e21bc85b1afe0b90bc7b599dd707e
chirags@google.com <chirags@google.com@6c5fc399-1a2c-022f-82ce-feb0d0b4916c>
2012-03-08 15:47:34 +0000

    Bump version for the 0.5.0 release.
    git-svn-id: http://google-api-php-client.googlecode.com/svn/trunk@378 6c5fc399-1a2c-022f-82ce-feb0d0b4916c

--- makeRelease.sh
+++ makeRelease.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-VERSION=0.4.9
+VERSION=0.5.0
 TMPDIR=/tmp/google-api-php-client
 RELFILE=/tmp/google-api-php-client-${VERSION}.tar.gz
 
--- src/io/apiHttpRequest.php
+++ src/io/apiHttpRequest.php
@@ -24,7 +24,7 @@
  *
  */
 class apiHttpRequest {
-  const USER_AGENT_SUFFIX = "google-api-php-client/0.4.8";
+  const USER_AGENT_SUFFIX = "google-api-php-client/0.5.0";
 
   protected $url;
   protected $requestMethod;
